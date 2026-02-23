ecc5c1749e62b8293b2f27ec9589a06e71a04e32
Shrestha Chandra <shrstha.git@gmail.com>
2026-02-23 23:40:23 +0530

    sandbox v1.0 update

--- client/src/pages/Events.tsx
+++ client/src/pages/Events.tsx
@@ -79,6 +79,7 @@ const EventsPage = () => {
         'The ultimate 24-hour international hackathon returns! Join brilliant minds from around the globe to build innovative solutions, compete for amazing prizes, and push the boundaries of technology.',
       missionId: 'RECURZIVE-V2',
       showCTA: true,
+      ctaLink: '/sandbox',
       icon: Terminal,
       color: {
         primary: 'text-cyan-400',
